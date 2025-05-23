cc4352556744046ac5f0c9e784f586d830533e07
heebraim <heebraim>
2025-05-23 10:02:00 -0500

    first commit

---
+++ home/main.js
@@ -0,0 +1,17 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
+
+
+toTop.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" 
+    });
+});
